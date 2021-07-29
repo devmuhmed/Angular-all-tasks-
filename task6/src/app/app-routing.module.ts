2f6b3a64bf5e07dd7ac0395feea0a0f500c6e123
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-07-29 08:13:44 +0200

    routes by using router outlet and make change on editprofile for responsive

--- task6/src/app/app-routing.module.ts
+++ task6/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditprofileComponent } from './editprofile/editprofile.component';
+import { ProfileComponent } from './profile/profile/profile.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"profile",component:ProfileComponent},
+  {path:"setting",component:SkillComponent},
+  {path:"editProfile",component:EditprofileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
