3b5dc1f6512d98d1deb65b29842950ef4b8cc03e
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-07-30 17:37:39 +0200

    binding data on editprofile

--- task6/src/app/editprofile/editprofile.component.ts
+++ task6/src/app/editprofile/editprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-editprofile',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
-
   constructor() { }
-
+  first_name:String=""
+  last_name:String=""
+  country:string="Egypt"
+  language=["arabic","English"]
+  gender=["male","female"]
+  show(d:NgForm){
+    console.log(d);
+  }
   ngOnInit(): void {
   }
 
